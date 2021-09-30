d8a6b42eafbb3435f6183c6b65282fb9c15f67e7
hugobanola99 <hugo.banola101@alu.ulpgc.es>
2021-09-30 17:34:08 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/practica3/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.practica3;
+
+/**
+ *
+ * @author hugob
+ */
+public class Main {
+    public static void main(String[] args) {
+        Practica3 frame = new Practica3();
+        frame.setVisible(true);
+    }
+    
+}
